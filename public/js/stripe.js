4852e81e7b88f91bda055e1f60c6bc701a98412a
Aicha Louraghi <a.louraghi@esi-sba.dz>
2022-10-09 00:18:00 +0200

    adding stripe js file

---
+++ public/js/stripe.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+import axios from 'axios';
+/* global Stripe */
+const stripe = Stripe(
+  'pk_test_51Lq1EqJr2ogVrP88jSRryEfu8l6TabyzTpU1UNDNA0H47JlYUO1tVd2DWuYaAdOgTj2uZYGbxuGdXatSBbOU1Mp6008QOjWFXj'
+);
+import { showAlert } from './alert';
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
