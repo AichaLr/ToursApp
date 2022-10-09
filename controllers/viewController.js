25e7a1becfbb048ca6e8058a1533863e21fc7b1e
Aicha Louraghi <a.louraghi@esi-sba.dz>
2022-10-10 00:13:41 +0200

    add getlogin  ,updateUserData,getMyBookings  functions

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
+const Booking = require('./../models/bookingModel');
 
 exports.getOverview = async (req, res) => {
   // 1) Get tour data from collection
@@ -20,7 +21,51 @@ exports.getTour = async (req, res) => {
     fields: 'review rating user',
   });
   res.status(200).render('tour', {
-    title: 'Tour Details',
+    title: `${tour.name} Tour`,
     tour,
+  });
+};
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account',
+  });
+};
+
+exports.updateUserData = async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser,
+  });
+};
+
+exports.getMyBookings = async (req, res, next) => {
+  const bookings = await Booking.find({ user: req.user.id });
+
+  const tourIds = bookings.map((el) => el.tour);
+
+  const tours = await Tour.find({ _id: { $in: tourIds } });
+
+  res.status(200).render('overview', {
+    title: 'My Bookings',
+    tours,
   });
 };
