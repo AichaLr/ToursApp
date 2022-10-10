84f860e0c323ab312350e45c58a54114d552e5ae
Aicha Louraghi <a.louraghi@esi-sba.dz>
2022-10-11 00:38:13 +0200

    update view route

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const viewController = require('./../controllers/viewController');
 const authController = require('./../controllers/authController');
+const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewController.getOverview);
+router.get(
+  '/',
+  bookingController.createCheckout,
+  authController.isLoggedIn,
+  viewController.getOverview
+);
 // when we have just one thing to export we use module.export=the thing
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 // when we have just one thing to export we use module.export=the thing
@@ -12,6 +18,7 @@ router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 // router.get('/logout', authController.logout);
 router.get('/me', authController.protect, viewController.getAccount);
+router.get('/my-tours', authController.protect, viewController.getMyBookings);
 //in case of form
 router.post(
   '/submit-user-data',
