009c6a37d1577709e096947d32065b3d9856101b
Aicha Louraghi <a.louraghi@esi-sba.dz>
2022-12-09 17:26:21 +0100

    Update tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,28 +8,6 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-// its a param  middleware  (the return exit the code if wedont use it the mddleware will send the response but it wll continue the code and execute next())
-/*exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-  next();
-};
-*/
-/*exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};*/
 
 exports.getAllTours = async (req, res) => {
   try {
